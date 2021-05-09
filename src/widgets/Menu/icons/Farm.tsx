175b7384298e2d7769b4c7c5216b02ff27965c9a
DrScatman <milleja1@mail.gvsu.edu>
2021-05-09 17:56:15 -0400

    polydragon

--- src/widgets/Menu/icons/Farm.tsx
+++ src/widgets/Menu/icons/Farm.tsx
@@ -4,9 +4,9 @@ import { SvgProps } from "../../../components/Svg/types";
 
 const Icon: React.FC<SvgProps> = (props) => {
   return (
-    <Svg viewBox="0 0 24 24" {...props}>
-      <image height={90} width={90} href="images/fire/forge.png" />
-    </Svg>
+    // <Svg viewBox="0 0 24 24" {...props}>
+    <image height={90} width={90} href="images/fire/forge.png" />
+    // </Svg>
   );
 };
 
