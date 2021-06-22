5697505d43e886a7826447fe8bcd5c625379ad0f
polydragondefi <bllitzer@yahoo.com>
2021-06-22 19:39:07 -0400

    glass

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -10,7 +10,7 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
     <Svg viewBox="0 0 220 50" {...props}>
-      <image width="220" height="50" href={isDark ? "/images/fire/title.png" : "/images/fire/title-light.png"} />
+      <image width="220" height="50" href={isDark ? "/images/glass/title.png" : "/images/glass/title-light.png"} />
     </Svg>
   );
 };
