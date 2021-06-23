1c47902393f84119adbe15c203f363a955d35203
polydragondefi <bllitzer@yahoo.com>
2021-06-22 20:25:46 -0400

    default

--- dist/components/Svg/Icons/Logo.d.ts
+++ dist/components/Svg/Icons/Logo.d.ts
@@ -1,7 +1,7 @@
 import React from "react";
 import { SvgProps } from "../types";
 interface IconProps extends SvgProps {
-    isDark: boolean;
+    isDark?: boolean;
 }
 declare const Icon: React.FC<IconProps>;
 export default Icon;
