60c735fd653973889c0a39c03aab6fe138414a0a
DrManhattan115 <milleja1@mail.gvsu.edu>
2021-09-24 13:03:52 -0400

    fix account link

--- src/widgets/WalletModal/AccountModal.tsx
+++ src/widgets/WalletModal/AccountModal.tsx
@@ -23,7 +23,7 @@ const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null
       {account}
     </Text>
     <Flex mb="32px">
-      <LinkExternal small href={`https://explorer-mainnet.maticvigil.com/address/${account}`} mr="16px">
+      <LinkExternal small href={`https://bscscan.com/address/${account}`} mr="16px">
         View on Explorer
       </LinkExternal>
       <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
